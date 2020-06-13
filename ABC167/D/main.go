d6805cf177e48f751e2b1a5d7d7cc7ea15d3429a
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-06-13 14:34:33 +0900

    add ABC167/D

---
+++ ABC167/D/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/youyouryu/AtCoder/lib"
+)
+
+func main() {
+	sc := lib.NewScanner(os.Stdin)
+	n, _ := sc.NextInt()
+	k, _ := sc.NextInt()
+	a, _ := sc.NextInts(n)
+	ans := solve(n, k, a)
+	fmt.Println(ans)
+}
+
+func solve(n, k int, a []int) (ans int) {
+	history := make([]int, n)
+	for i := range history {
+		history[i] = -1
+	}
+	current := 1
+	history[current-1] = k
+	for k > 0 {
+		current = a[current-1]
+		k--
+		if history[current-1] != -1 {
+			periodicTime := k - history[current-1]
+			k %= periodicTime
+		}
+		history[current-1] = k
+	}
+	return current
+}
