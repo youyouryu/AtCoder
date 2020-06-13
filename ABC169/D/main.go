81b8b69a281cd1e843815b2ea9aa5c34a8275276
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-06-14 01:09:47 +0900

    add ABC169/D

---
+++ ABC169/D/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/youyouryu/AtCoder/lib"
+)
+
+func main() {
+	sc := lib.NewScanner(os.Stdin)
+	n, _ := sc.NextInt()
+	ans := solve(n)
+	fmt.Println(ans)
+}
+
+func solve(n int) (ans int) {
+	for i := 2; i*i <= n; i++ {
+		for z := i; n%z == 0; z *= i {
+			ans++
+			n /= z
+		}
+		for n%i == 0 {
+			n /= i
+		}
+	}
+	if n != 1 {
+		ans++
+	}
+	return ans
+}
