fc768e0319059a4398880322ad34acbfa63bb4cb
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-06-20 18:27:03 +0900

    add ABC169/F

---
+++ ABC169/F/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/youyouryu/AtCoder/lib"
+)
+
+func main() {
+	sc := lib.NewScanner(os.Stdin)
+	n, _ := sc.NextInt()
+	s, _ := sc.NextInt()
+	a, _ := sc.NextInts(n)
+	ans := solve(n, s, a)
+	fmt.Println(ans)
+}
+
+const mod = 998244353
+
+func solve(n, s int, a []int) (ans int) {
+	dp, prev := make([]int, s+1), make([]int, s+1)
+	dp[0] = 1
+	for i := 0; i < n; i++ {
+		copy(prev, dp)
+		for j := 0; j <= s; j++ {
+			dp[j] = 2 * prev[j]
+			dp[j] %= mod
+			if j-a[i] >= 0 {
+				dp[j] += prev[j-a[i]]
+				dp[j] %= mod
+			}
+		}
+	}
+	return dp[s]
+}
