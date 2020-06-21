f6a49889ad5ee6580aa6804965f28fc99eb42fa2
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-06-22 00:08:21 +0900

    add lib Io

--- lib/io.go
+++ lib/io.go
@@ -3,9 +3,12 @@ package lib
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"io"
 	"math"
+	"os"
 	"strconv"
+	"strings"
 )
 
 // Scanner is a wrapper of bufio.Scanner which is customized for competitive programing.
@@ -48,4 +51,89 @@ func (sc *Scanner) NextString() (string, error) {
 		return "", errors.New("failed to scan")
 	}
 	return sc.Text(), nil
+}
+
+// Io is I/O object
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+// NewIo returns a new Io instance
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+// Flush flushes writer
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// NextLine scans a line from stdin
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+// Next return a word from stdin
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+// NextInt returns an integer from stdin
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+// NextFloat returns a float64 number from stdin
+func (io *Io) NextFloat() float64 {
+	f, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+// Println is a wrapper of fmt.Fprintln
+func (io *Io) Println(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+// Printf is awapper of fmt.Fprintf
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+// Log outputs a to stderr
+func Log(a ...interface{}) {
+	fmt.Fprintln(os.Stderr, a...)
 }
