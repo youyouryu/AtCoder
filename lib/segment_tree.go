0d823c9e1d82c4f8f02b2ff750854655a6083b28
Yu Yamada <yuyamada@MacBookPro2019.local>
2020-07-09 17:12:06 +0900

    fix segment tree

--- lib/segment_tree.go
+++ lib/segment_tree.go
@@ -2,15 +2,15 @@ package lib
 
 import "fmt"
 
-// MergeFunc merges two nodes to one.
+// MergeFunc merges two.Nodes to one.
 type MergeFunc func(interface{}, interface{}) interface{}
 
 // SegmentTree is data structure which can calculate the representative value of section
 type SegmentTree struct {
 	Size      int
 	Offset    int
-	nodes     []interface{}
-	mergeFunc MergeFunc
+	Nodes     []interface{}
+	MergeFunc MergeFunc
 }
 
 // NewSegmentTree returns a new initialized instance of SegmentTree
@@ -20,28 +20,28 @@ func NewSegmentTree(data []interface{}, mergeFunc MergeFunc) *SegmentTree {
 		depth++
 	}
 	size := 1<<(depth+1) - 1
-	nodes := make([]interface{}, size)
+	Nodes := make([]interface{}, size)
 	offset := 1<<depth - 1
 	for i := offset; i < offset+len(data); i++ {
-		nodes[i] = data[i-offset]
+		Nodes[i] = data[i-offset]
 	}
 	s := &SegmentTree{
 		Size:      size,
 		Offset:    offset,
-		nodes:     nodes,
-		mergeFunc: mergeFunc,
+		Nodes:     Nodes,
+		MergeFunc: mergeFunc,
 	}
 	return s.fix()
 }
 
 func (s *SegmentTree) fix() *SegmentTree {
 	for i := s.Offset - 1; i >= 0; i-- {
-		left, right := s.nodes[2*i+1], s.nodes[2*i+2]
+		left, right := s.Nodes[2*i+1], s.Nodes[2*i+2]
 		if right == nil {
-			s.nodes[i] = left
+			s.Nodes[i] = left
 			continue
 		}
-		s.nodes[i] = s.mergeFunc(left, right)
+		s.Nodes[i] = s.MergeFunc(left, right)
 	}
 	return s
 }
@@ -55,7 +55,7 @@ func (s *SegmentTree) calc(begin, end, k, cBegin, cEnd int) interface{} {
 	if cEnd <= begin || end <= cBegin {
 		return nil
 	} else if begin <= cBegin && cEnd <= end {
-		return s.nodes[k]
+		return s.Nodes[k]
 	}
 	c1 := s.calc(begin, end, 2*k+1, cBegin, (cBegin+cEnd)/2)
 	c2 := s.calc(begin, end, 2*k+2, (cBegin+cEnd)/2, cEnd)
@@ -64,12 +64,12 @@ func (s *SegmentTree) calc(begin, end, k, cBegin, cEnd int) interface{} {
 	} else if c2 == nil {
 		return c1
 	}
-	return s.mergeFunc(c1, c2)
+	return s.MergeFunc(c1, c2)
 }
 
 // Get returns a node
 func (s *SegmentTree) Get(index int) interface{} {
-	return s.nodes[index]
+	return s.Nodes[index]
 }
 
 // Top returns the root node
@@ -79,14 +79,14 @@ func (s *SegmentTree) Top() interface{} {
 
 // Update updates a leaf node and reconstruct tree structure.
 func (s *SegmentTree) Update(index int, value interface{}) *SegmentTree {
-	s.nodes[index] = value
+	s.Nodes[index] = value
 	return s.fix()
 }
 
 // Print outputs contens of the tree for debug
 func (s *SegmentTree) Print() {
 	fmt.Printf("offset=%d\n", s.Offset)
-	for i, v := range s.nodes {
+	for i, v := range s.Nodes {
 		fmt.Printf("%d %+v\n", i, v)
 	}
 	fmt.Println()
